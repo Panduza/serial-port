cda0f8af661e91fd8a309fa44fbecfe76af67566
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2025-10-30 06:38:43 +0100

    dev

--- src/drivers/emulator.rs
+++ src/drivers/emulator.rs
@@ -34,7 +34,24 @@ impl SerialPortDriver for PowerSupplyEmulator {
     async fn initialize(&mut self, mqtt_client: RumqttCustomAsyncClient) -> anyhow::Result<()> {
         info!("Emulator Driver: initialize");
 
-        self.client = Some(mqtt_client);
+        self.client = Some(mqtt_client.clone());
+
+        // Spawn a task to periodically send test data on the rx topic
+        tokio::spawn(async move {
+            let mut counter = 0u32;
+            loop {
+                tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+
+                let test_message = format!("Emulator test message #{}\n", counter);
+                let topic = mqtt_client.topic_with_prefix("rx");
+
+                if let Err(e) = mqtt_client.publish(topic, test_message.into_bytes()).await {
+                    tracing::error!("Failed to publish emulator test message: {}", e);
+                }
+
+                counter += 1;
+            }
+        });
 
         Ok(())
     }
