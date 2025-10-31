57549bd868c0797c1c28236cfd8a4f4ffc8cdb5b
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2025-10-31 12:05:54 +0100

    adapt client

--- src/client/builder.rs
+++ src/client/builder.rs
@@ -2,14 +2,26 @@ use crate::client::SerialPortClient;
 use pza_toolkit::config::IPEndpointConfig;
 use pza_toolkit::rumqtt::client::init_client;
 
-#[derive(Default)]
 /// Builder pattern for creating SerialPortClient instances
 pub struct SerialPortClientBuilder {
-    /// Name of the power supply unit
-    pub psu_name: Option<String>,
+    /// Name of the instance
+    pub instance_name: Option<String>,
 
     /// MQTT broker configuration
     pub ip: Option<IPEndpointConfig>,
+
+    /// Enable transmission monitoring
+    pub enable_tx_monitoring: bool,
+}
+
+impl Default for SerialPortClientBuilder {
+    fn default() -> Self {
+        Self {
+            instance_name: None,
+            ip: None,
+            enable_tx_monitoring: false, // Explicitly set to false
+        }
+    }
 }
 
 impl SerialPortClientBuilder {
@@ -25,7 +37,12 @@ impl SerialPortClientBuilder {
 
     /// Set the power supply name for the client
     pub fn with_power_supply_name<A: Into<String>>(mut self, name: A) -> Self {
-        self.psu_name = Some(name.into());
+        self.instance_name = Some(name.into());
+        self
+    }
+
+    pub fn enable_tx_monitoring(mut self, enable: bool) -> Self {
+        self.enable_tx_monitoring = enable;
         self
     }
 
@@ -36,9 +53,10 @@ impl SerialPortClientBuilder {
         let (client, event_loop) = init_client("serial-port");
 
         Ok(SerialPortClient::new_with_client(
-            self.psu_name.unwrap(),
+            self.instance_name.unwrap(),
             client,
             event_loop,
+            self.enable_tx_monitoring,
         ))
     }
 }
